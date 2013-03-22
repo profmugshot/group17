d7af56d4242b3e46ed007f85a733e00ecf6ef725
Robin <robinlin@Robins-MacBook-Pro.local>
2013-03-22 02:05:56 -0700

    Add MySQL connector, change in web crawling
    Will crawl through some internal links and store them into MySQL DB. DB
    is called "storage" and the table is "docs".

--- crawler.py
+++ crawler.py
@@ -8,14 +8,39 @@ import pdb
 import os
 import re
 import urllib2
+import MySQLdb
 
 docDB={}
+#mysql connector
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+		     user="root", # your username
+		      passwd="test", # your password
+		      db="storage") # name of the data base
+cur = db.cursor()
+#cur.execute('SHOW DATABASES LIKE "storage";')
+cur.execute('SHOW TABLES;')
+db.commit()
+print "checking database"
+if len(cur.fetchall())==0: #database doesn't exist, needs to be created
+	print "db doesn't exist, creating one"
+	cur.execute( "create table docs ("+
+		     "docID VARCHAR(100) NOT NULL PRIMARY KEY,"+
+		     "title VARCHAR(100),"+
+		     "html TEXT,"+
+		     "lastmodified TIMESTAMP(6) );"
+		     );
+	db.commit()
+	cur.execute('SHOW DATABASES LIKE "storage";')
+	db.commit()
+	assert len(cur.fetchall())!=0, 'database fail to create'
+
+
 
 class doc(object):
-	def __init__(self, docID, webURL, title):
+	def __init__(self, docID, title, html):
 		self.docID = docID
-		self.url = webURL
 		self.title = title
+		self.html = html
 	def setHTML(self, html): #gets the entire HTML doc
 		self.html = html
 	def getHTML(self):
@@ -45,8 +70,15 @@ def visible(element):
 
 def crawl():
 	seedURL = ['http://www.cs.sfu.ca/people/faculty.html']
-
-	for url in seedURL:	
+	parsedURL = []
+	while len(seedURL)!=0:
+		url = seedURL.pop(0) #gets first element
+		if (url in parsedURL):
+			print 'URL {0} has been visited'.format(url)
+			continue
+		else:
+			parsedURL.append(url)
+		print 'Reading URL %s', url
 		print 'Parsing seed URL'
 		html = urllib2.urlopen(url)
 		soup = BeautifulSoup(html)
@@ -55,47 +87,58 @@ def crawl():
 		#seedURL = seedURL + tempList
 				
 
-		pageTitle = soup.title.string
-		docID = randint(10, 9999999) #random id for docID
-		docObj = doc(docID, url, pageTitle)
+		pageTitle = clean(soup.title.string)
+		#docID = randint(10, 9999999) #random id for docID
+		docID = url
+		docObj = doc(docID, pageTitle, html)
 	
-		if docID not in docDB:#check if doc ID is unique
-			docDB[docID]=docObj
-		else:
-			docID = randint(9999999,9999999999)#pick something else
-			docDB[docID]=docObj	
-		docDB[docID].setHTML(soup)
-
+		#if docID not in docDB:#check if doc ID is unique
+		#	docDB[docID]=docObj
+		#else:
+		#	docID = randint(9999999,9999999999)#pick something else
+		#	docDB[docID]=docObj	
+		#docDB[docID].setHTML(soup)
+		sql = 'insert into docs (docID, title, html) values ("%s", "%s", "%s");'
+		cur.execute(sql, (docID, pageTitle, soup) )
+	
+		
 		#print clean(docDB[docID].getTitle())
 		#print docDB[docID].getHTML()
-		
+		try:
+			db.commit()
+		except:
+			print 'duplicates. check sql'
 		#text = soup.findAll(text=True)
 		#page = filter(visible, text)
 		#print page
+		seedURL = seedURL + tempList
 
 def extractInternalLinks(parentSoup):
 	links=[]
 
 	for link in parentSoup.find_all('a'):
 		link = link.get('href')
-		if not 'cs.sfu.ca' in link:
-			continue
-
 		try:
-			html = urllib2.urlopen(link) #pulls html
-			parsedPage = BeautifulSoup(html)
+			if not('cs.sfu.ca' in link.lower()):
+				continue
+			try:
+				html = urllib2.urlopen(link) #pulls html
+				parsedPage = BeautifulSoup(html)
+			except:
+				print 'cant open page' + link 
+				continue
+		
+			text = parsedPage.findAll(text=True)
+			page = filter(visible, text)
+			page = [token.strip(' ').lower() for token in page]
+			
+			relevance = checkRelevance(page)
+			print "Page with URL {0} is {1}.".format(link, "relevant" if relevance else "not relevant")
+			if relevance:
+				links.append(link)
 		except:
-			print 'cant open page' + link 
-			continue
-		
-		text = parsedPage.findAll(text=True)
-		page = filter(visible, text)
-		page = [token.strip(' ').lower() for token in page]
-		
-		relevance = checkRelevance(page)
-		print "Page with URL {0} is {1}.".format(link, "relevant" if relevance else "not relevant")
-		if relevance:
-			links.append(link)
+			print 'empty link'
+			
 		#	if link.startswith('/'): #deals with internal links eg: /people/faculty.html
 		#		link = 'http://www.cs.sfu.ca'+link.get('href')
 		#		links.append(link)
