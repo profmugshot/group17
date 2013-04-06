ee7793e856b7833ecfc4547c664f8c4c8a1355e4
theMajc <max.majc@gmail.com>
2013-04-05 21:52:53 -0700

    Update search.py
    test search

--- www/cgi-bin/search.py
+++ www/cgi-bin/search.py
@@ -5,7 +5,20 @@ import jinja2 as jj
 import os
 import mod_locator
 import cgi, cgitb
+import MySQLdb
 
+
+#mysql connector
+#--------------------------------------------
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+user="root", # your username
+passwd="ihave1cookie", # your password
+db="storage") # name of the data base
+#--------------------------------------------
+cur = db.cursor()
+
+
+#============================================
 DEBUG = 0
 
 path=mod_locator.mod_path()
@@ -17,12 +30,27 @@ cgitb.enable()
 fs = cgi.FieldStorage()
 querys = fs.getlist("query")
 query = ",".join(querys)
-query = query.replace(","," ")
+form = query.replace(","," ")
+query = query.strip(",")
+
+
+tokenDocList = []
+for token in query:
+    sql = 'select docID from indexterms where terms=%s;'
+    cur.execute(sql, token)
+    db.commit()
+    tokenList = cur.fetchall()
+    tokenDocList.append(tokenList)
+
+result = set(tokenDocList[0]).intersection(*tokenDocList)
+result = list(result)
 
 var = {
     'title': 'CS456 G17 Jinja2 - '+query,
     'heading': 'Jinja Demo',
-    'query': query
+    'query': form,
+    'results': result,
+    'resultLen':len(result)
     }
 
 template = env.get_template('search.html')
@@ -30,3 +58,7 @@ print "Content-type:text/html\r\n\r\n"
 print template.render(var)
 
 if DEBUG: print query
+
+print len(result)
+for i in result:
+print i
