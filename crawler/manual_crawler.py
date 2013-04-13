8d6a622d323479ef86c3402e7185d1ced90a86e2
profmugshot <rl.vlux@gmail.com>
2013-04-12 22:27:51 -0700

    update manual crawler and indexer

--- crawler/manual_crawler.py
+++ crawler/manual_crawler.py
@@ -63,7 +63,10 @@ def visible(element):
 	return True
 
 def crawl():
-	seedURL = ['http://www.cs.sfu.ca/people/faculty.html','http://www.cs.sfu.ca/CourseCentral']
+	profStr = ''' 
+http://www.cs.sfu.ca/people/faculty/gregbaker.html http://www.cs.sfu.ca/~ggbaker/ http://www.cs.sfu.ca/people/faculty/bradleybart.html http://www.cs.sfu.ca/~bbart/ http://www.cs.sfu.ca/people/faculty/petraberenbrink.html http://www.cs.sfu.ca/~petra/ http://www.cs.sfu.ca/people/faculty/binaybhattacharya.html http://www.cs.sfu.ca/~binay/ http://www.cs.sfu.ca/people/faculty/andreibulatov.html http://www.cs.sfu.ca/~abulatov/ http://www.cs.sfu.ca/people/faculty/robertdcameron.html http://www.cs.sfu.ca/~cameron/ http://www.cs.sfu.ca/people/faculty/dianacukierman.html http://www.cs.sfu.ca/people/faculty/ryandarcy.html http://www.cs.sfu.ca/%7Everonica/ http://www.cs.sfu.ca/people/faculty/veronicadahl1.html http://www.cs.sfu.ca/~veronica/ http://www.cs.sfu.ca/people/faculty/jamesdelgrande.html http://www.cs.sfu.ca/~jim/ http://www.cs.sfu.ca/people/faculty/tonydixon.html http://www.cs.sfu.ca/people/faculty/tobydonaldson.html http://www.cs.sfu.ca/people/faculty/markdrew.html http://www.cs.sfu.ca/~mark/ http://www.cs.sfu.ca/people/faculty/johnedgar.html http://www.cs.sfu.ca/people/faculty/fundaergun.html http://www.cs.sfu.ca/~funda/ http://www.cs.sfu.ca/people/faculty/martinester.html http://www.cs.sfu.ca/~ester/ http://www.cs.sfu.ca/people/faculty/mikeevans.html http://www.cs.sfu.ca/people/faculty/alexandrafedorova.html http://www.cs.sfu.ca/~fedorova/ http://www.cs.sfu.ca/people/faculty/brianfraser.html http://www.cs.sfu.ca/~bfraser/ http://www.cs.sfu.ca/people/faculty/brianfunt.html http://www.cs.sfu.ca/~funt/ http://www.cs.sfu.ca/people/faculty/uweglasser.html http://www.cs.sfu.ca/~glaesser/ http://www.cs.sfu.ca/people/faculty/QianpingGu.html http://www.cs.sfu.ca/~qgu/ http://www.cs.sfu.ca/people/faculty/louhafer.html http://www.cs.sfu.ca/~lou/ http://www.cs.sfu.ca/people/faculty/ghassanhamarneh.html http://www.cs.sfu.ca/~hamarneh/ http://www.cs.sfu.ca/people/faculty/mohamedhefeeda.html http://www.cs.sfu.ca/~mhefeeda/ http://www.cs.sfu.ca/people/faculty/pavolhell.html http://www.cs.sfu.ca/~pavol/ http://www.cs.sfu.ca/people/faculty/valentinekabanets.html http://www.cs.sfu.ca/~kabanets/ http://www.cs.sfu.ca/people/faculty/harindersinghkhangura.html http://www.cs.sfu.ca/~hskhangu/ http://www.cs.sfu.ca/people/faculty/arthurkirkpatrick.html http://www.cs.sfu.ca/~ted/ http://www.cs.sfu.ca/people/faculty/RameshKrishnamurti.html http://www.cs.sfu.ca/~ramesh/ http://www.cs.sfu.ca/people/faculty/annelavergne.html http://www.cs.sfu.ca/~alavergn/ http://www.cs.sfu.ca/people/faculty/ze-lianli.html http://www.cs.sfu.ca/~li/ http://www.cs.sfu.ca/people/faculty/arthurliestman.html http://www.cs.sfu.ca/~art/ http://www.cs.sfu.ca/people/faculty/jiangchuanliu.html http://www.cs.sfu.ca/~jcliu/ http://www.cs.sfu.ca/people/faculty/Wo-ShunLuk.html http://www.cs.sfu.ca/~woshun/ http://www.cs.sfu.ca/people/faculty/davidmitchell.html http://www.cs.sfu.ca/~mitchell/ http://www.cs.sfu.ca/people/faculty/gregmori.html http://www.cs.sfu.ca/~mori/ http://www.cs.sfu.ca/people/faculty/torstenmoller.html http://www.cs.sfu.ca/~torsten/ http://www.cs.sfu.ca/people/faculty/stevenpearce.html http://www.cs.sfu.ca/~stevenp/ http://www.cs.sfu.ca/people/faculty/jianpei.html http://www.cs.sfu.ca/~jpei/ http://www.cs.sfu.ca/people/faculty/JosephPeters.html http://www.cs.sfu.ca/~peters/ http://www.cs.sfu.ca/people/faculty/fredpopowich.html http://www.cs.sfu.ca/~popowich/ http://www.cs.sfu.ca/people/faculty/janiceregan.html http://www.cs.sfu.ca/~jregan/ http://www.cs.sfu.ca/people/faculty/cenkssahinalp.html http://www.cs.sfu.ca/~cenk/ http://www.cs.sfu.ca/people/faculty/anoopsarkar.html http://www.cs.sfu.ca/~anoop/ http://www.cs.sfu.ca/people/faculty/oliverschulte.html http://www.cs.sfu.ca/~oschulte/ http://www.cs.sfu.ca/people/faculty/thomasshermer.html http://www.cs.sfu.ca/~shermer/ http://www.cs.sfu.ca/people/Faculty/Profile/ashriram.html http://www.cs.sfu.ca/people/faculty/ArrvindhShriraman.html http://www.cs.sfu.ca/~ashriram/ http://www.cs.sfu.ca/people/Faculty/Profile/tamaras.html http://www.cs.sfu.ca/people/faculty/tamarasmyth.html http://www.cs.sfu.ca/~tamaras/ http://www.cs.sfu.ca/people/Faculty/Profile/tardos.html http://www.cs.sfu.ca/people/faculty/gabortardos.html http://www.cs.sfu.ca/~tardos/ http://www.cs.sfu.ca/people/Faculty/Profile/ter.html http://www.cs.sfu.ca/people/faculty/EugeniaTernovska.html http://www.cs.sfu.ca/~ter/ http://www.cs.sfu.ca/people/Faculty/Profile/vaughan.html http://www.cs.sfu.ca/people/faculty/richardvaughan.html http://www.cs.sfu.ca/~vaughan/ http://www.cs.sfu.ca/people/Faculty/Profile/wangk.html http://www.cs.sfu.ca/people/faculty/kewang.html http://www.cs.sfu.ca/~wangk/ http://www.cs.sfu.ca/people/faculty/kaywiese.html http://www.cs.sfu.ca/~wiese/ http://www.cs.sfu.ca/people/faculty/cynthiaxie.html http://www.cs.sfu.ca/people/Faculty/Profile/haoz.html http://www.cs.sfu.ca/people/faculty/richardzhang.html http://www.cs.sfu.ca/~haoz/'''
+	seedURL = profStr.split(" ") 
+	#seedURL = ['http://www.cs.sfu.ca/people/faculty.html','http://www.cs.sfu.ca/CourseCentral']
 	parsedURL = []
 	while len(seedURL)!=0:
 ##		if len(parsedURL)>20:
@@ -81,7 +84,7 @@ def crawl():
 
 		soup = BeautifulSoup(html)
 		try:
-			extractedLinks = extractInternalLinks(url, soup)
+		#	extractedLinks = extractInternalLinks(url, soup)
 			pageTitle = clean(soup.title.string)
 		except:
 			print 'cannot clean page title, likely a PDF, skipping doc'
@@ -101,15 +104,15 @@ def crawl():
 			print 'duplicates. check sql'
 		parsedSet= Set(parsedURL)
 		seedSet = Set(seedURL)
-		tempSet = Set(extractedLinks)
+		#tempSet = Set(extractedLinks)
 
 		#get all urls in tempSet that is not in parsedSet (tempSet - parsedSet)
-		toBeAddedURL = tempSet.difference(parsedSet)
+		#toBeAddedURL = tempSet.difference(parsedSet)
 
-		seedSet = seedSet.union(toBeAddedURL)
+		#seedSet = seedSet.union(toBeAddedURL)
 		#seedURL = list(seedSet) #manually crawl seed URLs and end
 		print 'length of seed URL ', len(seedURL)
-		print 'length of parsedURL ', len(list(tempSet))
+		#print 'length of parsedURL ', len(list(tempSet))
 
 		print '#####ending iteration####'
 
