4e1cc87e8a88ca28ec0a1ee131ddc3eb66dbb318
Cheng <chengcao8@gmail.com>
2013-04-14 17:30:33 -0700

    professors and their courses are linked-ish
    test_beautiful_soup
    and newest_parser.py are working at their best
    
    go back to this if something goes wrong.

--- parsers/newest_parser.py
+++ parsers/newest_parser.py
@@ -1,38 +1,57 @@
 from bs4 import BeautifulSoup
-import sys
+import sys, re
 from urlparse import urljoin
 import urllib2
 import codecs
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                     passwd="", # your password
+                     db="project_final") # name of the data base
+cur = db.cursor()
 
 seed = "http://portal.cs.sfu.ca/"
-response = urllib2.urlopen('https://portal.cs.sfu.ca/outlines/1134/')
-html = response.read()
-soup = BeautifulSoup(html)
+terms = ['1137','1134','1131','1127','1124','1121','1117','1111','1107','1104','1101','1097','1094','1091','1087','1084','1081','1077','1074','1071','1067','1064','1061','1057','1054','1051','1047']
+#terms = ['1134']
+for termID in terms:
+    response = urllib2.urlopen('https://portal.cs.sfu.ca/outlines/'+str(termID))
+    html = response.read()
+    soup = BeautifulSoup(html)
 
-tabulka = soup.find("table", {"class" : "tablesorter"})
+    tabulka = soup.find("table", {"class" : "tablesorter"})
 
-records = [] # store all of the records in this list
-for row in tabulka.findAll('tr'):
-    col = row.findAll('td')
-    #outline_a = col.find_all("a")
-    try:
-        
-        print col[0].text.strip() 
-        print col[1].text.strip() 
-        print col[2].text.strip() 
-        print col[3].text.strip() 
-        print col[4].text.strip() 
-        print col[5].text.strip() 
-        print urljoin(seed,col[6].a['href'])
-    except:
-        print "broke"
-    print
-    #prvy = col[1].string.strip()
-    #druhy = col[1].string.strip()
-    #record = '%s;%s' % (prvy, druhy) # store the record with a ';' between prvy and druhy
-    #records.append(record)
+    records = [] # store all of the records in this list
+    row_id = 0
+    for row in tabulka.findAll('tr'):
+        col = row.findAll('td')
+        sys.stdout.write(termID+",")
+        if col:
+            subject = col[0].text.strip() 
+            number = col[1].text.strip() 
+            section = col[2].text.strip() 
+            title = col[3].text.strip() 
+            campus = col[4].text.strip() 
+            instructor = re.sub(r"\s\s+", ';', col[5].text.strip())
+            try:
+                outline=urljoin(seed,col[6].a['href'])
+            except:
+                outline=""
 
-fl = codecs.open('output_outlines.txt', 'wb', 'utf8')
-line = ';'.join(records)
-fl.write(line + u'\r\n')
-fl.close()
+            sql = '''
+            insert into courses (
+            subject, number, section, title, campus, instructor, outline,term)
+             values (%s, %s, %s, %s, %s, %s, %s, %s);
+            '''
+            cur.execute(sql, (subject, number, section, title, campus, instructor, outline, termID) )
+            db.commit()
+            #print "values (%s, %s, %s, %s, %s, %s, %s, %s)"%(subject, number, section, title, campus, instructor, outline, termID) 
+        #except:
+         #   print "broke on row: " + str(row_id)
+
+        row_id = row_id+1
+
+    #fl = codecs.open('output_outlines.txt', 'wb', 'utf8')
+    #line = ';'.join(records)
+    #fl.write(line + u'\r\n')
+    #fl.close()
--- parsers/test_beautiful_soup.py
+++ parsers/test_beautiful_soup.py
@@ -1,7 +1,16 @@
 import urllib2
 from bs4 import BeautifulSoup
-import sys
+import sys, re
 from urlparse import urljoin
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                     passwd="", # your password
+                     db="project_final") # name of the data base
+cur = db.cursor()
+
+
 seed = "http://www.cs.sfu.ca/"
 page = urllib2.urlopen("http://www.cs.sfu.ca/people/faculty.html")
 soup = BeautifulSoup(page)
@@ -20,6 +29,39 @@ for i in textimage:
     all_a =  info_block.find_all("a")
     all_p =  info_block.find_all("p")
     print all_p[0]
+    prof_name = ""
+    rx = re.compile('\W+')
+    try:
+        if info_block.h4:
+            prof_name=rx.sub('%',info_block.h4.text.split(",")[0]).strip()
+        
+    except:
+        print "some professor has a broken<h4> name"
+
+    try:
+        if info_block.h3:
+            prof_name=rx.sub('%',info_block.h3.text.split(",")[0]).strip()
+    except:
+        print "NO NAME?"
+    print "this is prof_name: " + prof_name
+    sql = '''
+            select subject,number,section,title,instructor from courses where instructor like %s;
+            '''
+    cur.execute(sql, prof_name )
+    rows = cur.fetchall()
+    db.commit()
+    if len(rows)<1:
+        cur.execute(sql, prof_name[0]+"%"+prof_name.split("%")[-1])
+        rows = cur.fetchall()
+        db.commit()
+    if len(rows)<1:
+        cur.execute(sql, "%"+prof_name.split("%")[-1])
+        rows = cur.fetchall()
+        db.commit()
+    
+    for row in rows:
+        print "taught: " + str(row)
+    
     
 
     for j in all_a:
