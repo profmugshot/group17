4db9ebdd3ab75530af6524e8f85e86119ed05ead
Cheng <chengcao8@gmail.com>
2013-04-17 15:34:41 -0700

    addign search

--- www/cgi-bin/prof_details.py
+++ www/cgi-bin/prof_details.py
@@ -73,43 +73,59 @@ tokenList = cur.fetchall()
 tokenDocList.append(tokenList)
 
 #freq count
-if FREQ_COUNT: 
-	resultDic={}
-	for token in tokenDocList:
-		#for url in result:
-		sql = "select pos, docID from indexterms where terms=%s;"
-		#cur.execute(sql, (token, url[0]))
-		cur.execute(sql, (token))
-		db.commit()
-		rows = (cur.fetchall())
-		pos = [freqIndex[0] for freqIndex in rows] #get all positions of all indexes and store in pos
-		posFreq = [len(freq.split(",")) for freq in pos] #split each positions into list and count them
-		#print posFreq
-		
-		i=0
-		for doc in rows: 
-			try:
-				#if doc is in dictionary already, exception thrown if trying to access non-existing element
-				val = resultDic[doc[1]] #frequency for doc[1]
-				#resultDic.update( {doc[1]:posFreq[i]+val} ) #appends to ditionary
-				resultDic[doc[1]]=val+1
-				
-			except:	
-				#if doc not in dictionary
-				resultDic.update( {doc[1]:posFreq[i]} ) #appends to ditionary	
-			i = i + 1
-	outputResultDic = {}
-	for docID in result:
-		docID=docID[0]
-		try: #if exist
-			val = resultDic[docID]
-			outputResultDic[docID] = val + 1 
-		except: #if not exist
-			outputResultDic[docID] = 1	
-			#print 'something went wrong with freq ', docID
-	result = sorted(outputResultDic.iteritems(), key=operator.itemgetter(1), reverse=True)
-		
-#print token
+query = tokenDocList
+result=[]
+if query:
+    for token in query:
+        sql = 'select docID from indexterms where terms=%s;'
+        cur.execute(sql, token)
+        db.commit()
+        tokenList = cur.fetchall()
+        tokenDocList.append(tokenList)
+
+    result = set(tokenDocList[0]).intersection(*tokenDocList)
+    result = list(result)
+    if DEBUG:
+        print "Length of result: %s" %len(result)
+        for i in result:
+            print i
+    #freq count
+    if FREQ_COUNT:
+        resultDic={}
+        for token in query:
+            #for url in result:
+            sql = "select pos, docID from indexterms where terms=%s;"
+            #cur.execute(sql, (token, url[0]))
+            cur.execute(sql, (token))
+            db.commit()
+            rows = (cur.fetchall())
+            pos = [freqIndex[0] for freqIndex in rows] #get all positions of all indexes and store in pos
+            posFreq = [len(freq.split(",")) for freq in pos] #split each positions into list and count them
+            #print posFreq
+
+            i=0
+            for doc in rows:
+                try:
+                    #if doc is in dictionary already, exception thrown if trying to access non-existing element
+                    val = resultDic[doc[1]] #frequency for doc[1]
+                    #resultDic.update( {doc[1]:posFreq[i]+val} ) #appends to ditionary
+                    resultDic[doc[1]]=val+1
+
+                except:
+                    #if doc not in dictionary
+                    resultDic.update( {doc[1]:posFreq[i]} ) #appends to ditionary
+                i = i + 1
+        outputResultDic = {}
+        for docID in result:
+            docID=docID[0]
+            try: #if exist
+                val = resultDic[docID]
+                outputResultDic[docID] = val + 1
+            except: #if not exist
+                outputResultDic[docID] = 1
+                #print 'something went wrong with freq ', docID
+        result = sorted(outputResultDic.iteritems(), key=operator.itemgetter(1), reverse=True)
+
 #print result
 
 #print "this is the token list: " + str(tokenList[0][2])
@@ -130,7 +146,8 @@ var = {
     'title': 'CS456 G17 Jinja2 - '+querys[0],
     'query': querys[0],
     'prof': tokenDocList,
-	'rows': rows
+	'rows': rows,
+    'results': result
     }
 
 ##
