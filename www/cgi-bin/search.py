97ce13d13482e607fa490d6a2f140203a92fe4b8
Max Ma <max.majc@gmail.com>
2013-04-17 04:55:04 -0700

    add null safe

--- www/cgi-bin/search.py
+++ www/cgi-bin/search.py
@@ -86,13 +86,14 @@ env = jj.Environment(loader=jj.FileSystemLoader(path+'/template'))
 
 ##
 # Getting html form POST
+query=['Jian','Pei']
 fs = cgi.FieldStorage()
 querys = fs.getlist("query")
 try:
     query = querys[0].split(" ")
 except:
     if DEBUG: print "No Query"
-
+    querys=['Jian Pei']
 if DEBUG: print query
 
 ##
@@ -120,128 +121,130 @@ sys.stdout.flush()
 ##
 # Retrieving from database
 tokenDocList = []
-for token in query:
-    sql = 'select docID from indexterms where terms=%s;'
-    cur.execute(sql, token)
-    db.commit()
-    tokenList = cur.fetchall()
-    tokenDocList.append(tokenList)
-
-result = set(tokenDocList[0]).intersection(*tokenDocList)
-result = list(result)
-if DEBUG:
-    print "Length of result: %s" %len(result)
-    for i in result:
-        print i
-				
-if DO_RANK:
-    rankLen = float(len(result))
-    rankCnt = 0
-    rankProgress=0.0
-    resultDict={}
-    for docID in result:
-        rankCnt += 1
-        print env.get_template('searchProgress.html').render(prog=rankCnt/rankLen*100)
-        sys.stdout.flush()
-        ##
-        # find the length of the docID
-        sql='select html from docs where docID=%s;'
-        cur.execute(sql, docID[0])
+result=[]
+if query:
+    for token in query:
+        sql = 'select docID from indexterms where terms=%s;'
+        cur.execute(sql, token)
         db.commit()
-        html_data = cur.fetchone()
+        tokenList = cur.fetchall()
+        tokenDocList.append(tokenList)
 
-        ##
-        # clean the parsed data
-        try:
-            page = html_data[0] #gets page
-        except:
-            continue
-        parsedPage = BeautifulSoup(html_data[0])
-        text = parsedPage.findAll(text=True)
-        page = filter(visible, text)
-        page = [token.strip(' ').lower() for token in page]
-        page = [token.split(' ') for token in page]
-        page = list(itertools.chain.from_iterable(page))
+    result = set(tokenDocList[0]).intersection(*tokenDocList)
+    result = list(result)
+    if DEBUG:
+        print "Length of result: %s" %len(result)
+        for i in result:
+            print i
 
-        size_of_bucket = len(page)
-
-        the_bucket = bucket(size_of_bucket)
-
-        print "</br>"
-        #dictionary to store docID plus max bucket and sort
-        for aToken in query:
-            if DEBUG: print "FOR THIS DOCID: "
-            if DEBUG: print docID
-            sql='select pos from indexterms where terms=%s AND docID=%s;'
-            cur.execute(sql, (aToken,docID[0]))
+    if DO_RANK:
+        rankLen = float(len(result))
+        rankCnt = 0
+        rankProgress=0.0
+        resultDict={}
+        for docID in result:
+            rankCnt += 1
+            print env.get_template('searchProgress.html').render(prog=rankCnt/rankLen*100)
+            sys.stdout.flush()
+            ##
+            # find the length of the docID
+            sql='select html from docs where docID=%s;'
+            cur.execute(sql, docID[0])
             db.commit()
-            jian_data = cur.fetchall()
-            if DEBUG: print "this is..." + aToken + " data."
-            a = jian_data[0][0].split(',')
-            JIAN = [int(y) for y in a]
-            if DEBUG: print JIAN
+            html_data = cur.fetchone()
 
-            bucket_add(JIAN,the_bucket)
-            if DEBUG:
-                print "\n"
-                print "running bucket for docID: " + str(docID)
-                print "SCORE: "
-                bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
-                resultDict.update({docID:bucketScore})
-	    else:
-                bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
-                resultDict.update({docID:bucketScore})
+            ##
+            # clean the parsed data
+            try:
+                page = html_data[0] #gets page
+            except:
+                continue
+            parsedPage = BeautifulSoup(html_data[0])
+            text = parsedPage.findAll(text=True)
+            page = filter(visible, text)
+            page = [token.strip(' ').lower() for token in page]
+            page = [token.split(' ') for token in page]
+            page = list(itertools.chain.from_iterable(page))
 
-            #print "result for bucket is... " + str(bucket(JIAN, PEI))
-                print "</br>\n\n"
+            size_of_bucket = len(page)
 
-    #sort bucket results
-    result = sorted(resultDict.iteritems(), key=operator.itemgetter(1), reverse=True)
-    if DEBUG: print result
-    result = [sortedLinks[0] for sortedLinks in result]
-    #result = list(result)
-#freq count
-if FREQ_COUNT: 
-	resultDic={}
-	for token in query:
-		#for url in result:
-		sql = "select pos, docID from indexterms where terms=%s;"
-		#cur.execute(sql, (token, url[0]))
-		cur.execute(sql, (token))
-		db.commit()
-		rows = (cur.fetchall())
-		pos = [freqIndex[0] for freqIndex in rows] #get all positions of all indexes and store in pos
-		posFreq = [len(freq.split(",")) for freq in pos] #split each positions into list and count them
-		#print posFreq
-		
-		i=0
-		for doc in rows: 
-			try:
-				#if doc is in dictionary already, exception thrown if trying to access non-existing element
-				val = resultDic[doc[1]] #frequency for doc[1]
-				#resultDic.update( {doc[1]:posFreq[i]+val} ) #appends to ditionary
-				resultDic[doc[1]]=val+1
-				
-			except:	
-				#if doc not in dictionary
-				resultDic.update( {doc[1]:posFreq[i]} ) #appends to ditionary	
-			i = i + 1
-	outputResultDic = {}
-	for docID in result:
-		docID=docID[0]
-		try: #if exist
-			val = resultDic[docID]
-			outputResultDic[docID] = val + 1 
-		except: #if not exist
-			outputResultDic[docID] = 1	
-			#print 'something went wrong with freq ', docID
-	result = sorted(outputResultDic.iteritems(), key=operator.itemgetter(1), reverse=True)
-		
+            the_bucket = bucket(size_of_bucket)
+
+            print "</br>"
+            #dictionary to store docID plus max bucket and sort
+            for aToken in query:
+                if DEBUG: print "FOR THIS DOCID: "
+                if DEBUG: print docID
+                sql='select pos from indexterms where terms=%s AND docID=%s;'
+                cur.execute(sql, (aToken,docID[0]))
+                db.commit()
+                jian_data = cur.fetchall()
+                if DEBUG: print "this is..." + aToken + " data."
+                a = jian_data[0][0].split(',')
+                JIAN = [int(y) for y in a]
+                if DEBUG: print JIAN
+
+                bucket_add(JIAN,the_bucket)
+                if DEBUG:
+                    print "\n"
+                    print "running bucket for docID: " + str(docID)
+                    print "SCORE: "
+                    bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
+                    resultDict.update({docID:bucketScore})
+    	    else:
+                    bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
+                    resultDict.update({docID:bucketScore})
+
+                #print "result for bucket is... " + str(bucket(JIAN, PEI))
+                    print "</br>\n\n"
+
+        #sort bucket results
+        result = sorted(resultDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+        if DEBUG: print result
+        result = [sortedLinks[0] for sortedLinks in result]
+        #result = list(result)
+    #freq count
+    if FREQ_COUNT:
+    	resultDic={}
+    	for token in query:
+    		#for url in result:
+    		sql = "select pos, docID from indexterms where terms=%s;"
+    		#cur.execute(sql, (token, url[0]))
+    		cur.execute(sql, (token))
+    		db.commit()
+    		rows = (cur.fetchall())
+    		pos = [freqIndex[0] for freqIndex in rows] #get all positions of all indexes and store in pos
+    		posFreq = [len(freq.split(",")) for freq in pos] #split each positions into list and count them
+    		#print posFreq
+
+    		i=0
+    		for doc in rows:
+    			try:
+    				#if doc is in dictionary already, exception thrown if trying to access non-existing element
+    				val = resultDic[doc[1]] #frequency for doc[1]
+    				#resultDic.update( {doc[1]:posFreq[i]+val} ) #appends to ditionary
+    				resultDic[doc[1]]=val+1
+
+    			except:
+    				#if doc not in dictionary
+    				resultDic.update( {doc[1]:posFreq[i]} ) #appends to ditionary
+    			i = i + 1
+    	outputResultDic = {}
+    	for docID in result:
+    		docID=docID[0]
+    		try: #if exist
+    			val = resultDic[docID]
+    			outputResultDic[docID] = val + 1
+    		except: #if not exist
+    			outputResultDic[docID] = 1
+    			#print 'something went wrong with freq ', docID
+    	result = sorted(outputResultDic.iteritems(), key=operator.itemgetter(1), reverse=True)
+
 #print token
 #print result
 
 ##
-# Constructing variables to pass to HTML	
+# Constructing variables to pass to HTML
 var = {
     'title': 'CS456 G17 Jinja2 - '+querys[0],
     'query': querys[0],
