80ba5a2d52a8d8b200fdb7fe4b7fb1376247998a
Cheng <chengcao8@gmail.com>
2013-04-17 16:35:19 -0700

    53135

--- www/cgi-bin/prof_details.py
+++ www/cgi-bin/prof_details.py
@@ -64,10 +64,13 @@ sys.stdout.flush()
 ###
 #needs to clean the GET information, so that we only get int IDs
 
+tokenDocList = []
+
 sql = 'select * from professors where prof_id=%s;'
 cur.execute(sql, querys[0])
 db.commit()
 tokenList = cur.fetchall()
+tokenDocList.append(tokenList)
 
 prof_name = str(tokenList[0][2])
 
@@ -78,15 +81,10 @@ cur.execute(sql, prof_name )
 rows = cur.fetchall()
 db.commit()
 
-
-
-
-
 #freq count
 query = str(tokenList[0][1]).split(" ")
 print "THis is the query going in: " + query[0]
 
-tokenDocList={}
 result=[]
 if query:
     for token in query:
