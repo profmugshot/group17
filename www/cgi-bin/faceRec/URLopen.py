031e405d8a636151eeeb8ae0293f898f71aef719
Max Ma <max.majc@gmail.com>
2013-04-17 06:12:10 -0700

    update url downloader

--- www/cgi-bin/faceRec/URLopen.py
+++ www/cgi-bin/faceRec/URLopen.py
@@ -15,15 +15,6 @@ def save(url,path=""):
     except:
         return -1 #bad url
     f = open(path+file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    if file_size>1000000:
-        return -2
-    if showProg:
-        print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
     while True:
         buffer = u.read(block_sz)
         if not buffer:
@@ -31,9 +22,5 @@ def save(url,path=""):
 
         file_size_dl += len(buffer)
         f.write(buffer)
-        if showProg:
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            print status,
     f.close()
     return path+file_name
