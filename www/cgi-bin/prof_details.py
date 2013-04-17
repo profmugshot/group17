c1d00ffa49d686a1cfe09cb989d6aa6bfa062a87
Cheng <chengcao8@gmail.com>
2013-04-17 00:59:47 -0700

    adding into the new prof_details page

--- www/cgi-bin/prof_details.py
+++ www/cgi-bin/prof_details.py
@@ -65,98 +65,25 @@ sys.stdout.flush()
 
 ##
 # Name Card Search Results
-namecards = namecard.generate_cards(query)
+#namecards = namecard.generate_cards(query)
 
-print env.get_template('searchProf.html').render(namecards=namecards)
-sys.stdout.flush()
+#print env.get_template('searchProf.html').render(namecards=namecards)
+#sys.stdout.flush()
 
 ##
-# Retrieving from database
+# Retrieving prof name from database
 tokenDocList = []
 for token in query:
-    sql = 'select docID from indexterms where terms=%s;'
+    sql = 'select prof_name from professors where prof_id=%s;'
     cur.execute(sql, token)
     db.commit()
     tokenList = cur.fetchall()
     tokenDocList.append(tokenList)
 
-result = set(tokenDocList[0]).intersection(*tokenDocList)
-result = list(result)
-if DEBUG:
-    print "Length of result: %s" %len(result)
-    for i in result:
-        print i
-				
-if DO_RANK:
-    rankLen = float(len(result))
-    rankCnt = 0
-    rankProgress=0.0
-    resultDict={}
-    for docID in result:
-        rankCnt += 1
-        print env.get_template('searchProgress.html').render(prog=rankCnt/rankLen*100)
-        sys.stdout.flush()
-        ##
-        # find the length of the docID
-        sql='select html from docs where docID=%s;'
-        cur.execute(sql, docID[0])
-        db.commit()
-        html_data = cur.fetchone()
-
-        ##
-        # clean the parsed data
-        try:
-            page = html_data[0] #gets page
-        except:
-            continue
-        parsedPage = BeautifulSoup(html_data[0])
-        text = parsedPage.findAll(text=True)
-        page = filter(visible, text)
-        page = [token.strip(' ').lower() for token in page]
-        page = [token.split(' ') for token in page]
-        page = list(itertools.chain.from_iterable(page))
-
-        size_of_bucket = len(page)
-
-        the_bucket = bucket(size_of_bucket)
-
-        print "</br>"
-        #dictionary to store docID plus max bucket and sort
-        for aToken in query:
-            if DEBUG: print "FOR THIS DOCID: "
-            if DEBUG: print docID
-            sql='select pos from indexterms where terms=%s AND docID=%s;'
-            cur.execute(sql, (aToken,docID[0]))
-            db.commit()
-            jian_data = cur.fetchall()
-            if DEBUG: print "this is..." + aToken + " data."
-            a = jian_data[0][0].split(',')
-            JIAN = [int(y) for y in a]
-            if DEBUG: print JIAN
-
-            bucket_add(JIAN,the_bucket)
-            if DEBUG:
-                print "\n"
-                print "running bucket for docID: " + str(docID)
-                print "SCORE: "
-                bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
-                resultDict.update({docID:bucketScore})
-	    else:
-                bucketScore = calculate_the_bucket(the_bucket,size_of_bucket);
-                resultDict.update({docID:bucketScore})
-
-            #print "result for bucket is... " + str(bucket(JIAN, PEI))
-                print "</br>\n\n"
-
-    #sort bucket results
-    result = sorted(resultDict.iteritems(), key=operator.itemgetter(1), reverse=True)
-    if DEBUG: print result
-    result = [sortedLinks[0] for sortedLinks in result]
-    #result = list(result)
 #freq count
 if FREQ_COUNT: 
 	resultDic={}
-	for token in query:
+	for token in tokenDocList:
 		#for url in result:
 		sql = "select pos, docID from indexterms where terms=%s;"
 		#cur.execute(sql, (token, url[0]))
@@ -200,7 +127,6 @@ var = {
     'query': querys[0],
     'results': result,
     'resultLen':len(result),
-    'namecards': namecards
     }
 
 ##
