ac1f457535c2a6e5af3286e98060b2ede21187c1
Max Ma <max.majc@gmail.com>
2013-04-18 02:24:59 -0700

    debugging imge example

--- www/cgi-bin/faceRec/URLopen.py
+++ www/cgi-bin/faceRec/URLopen.py
@@ -1,6 +1,6 @@
 import urllib2
 showProg=0
-
+debug = 1
 def read(url):
     return urllib2.urlopen(url).read()
 
@@ -10,19 +10,22 @@ def read(url):
 ##  -2:file larger than 1M
 def save(url,path=""):
     file_name = url.split('/')[-1]
+    if debug: print "got url:%s"%(url)
+    if debug: print "filename:%s"%(file_name)
     try:
         u = urllib2.urlopen(url)
     except:
         return -1 #bad url
     f = open(path+file_name, 'wb')
+    if debug: print "saveing to %s"%(path+file_name)
     file_size_dl = 0
     block_sz = 8192
     while True:
         buffer = u.read(block_sz)
         if not buffer:
             break
-
         file_size_dl += len(buffer)
         f.write(buffer)
+    if debug: print "done."
     f.close()
     return path+file_name
